e9b4bc7043bd2a4cd7b7752da7d9c5e108430f56
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2021-02-08 18:29:16 +0100

    08/02/21_MAC

--- gcc.sh
+++ gcc.sh
@@ -4,3 +4,15 @@ test -f $2 && rm $2
 nasm -f macho64 hello.s
 gcc main.c $1 -o $2
 ./$2
+test -f $1 && rm $1
+test -f $2 && rm $2
+
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+if [ $? -eq 0 ]; then
+    echo ${green}SUCCESS
+else
+    echo ${red}FAIL
+fi
