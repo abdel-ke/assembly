fc60d580ba76997c0ebfb35e3e58bcb318f8523d
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2021-02-08 17:53:45 +0100

    ./g08/02/21

---
+++ git.sh
@@ -0,0 +1,4 @@
+git status
+git add .
+git commit -m "./g$1"
+git push
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int        ft_strlen(char *str);
 int main() {
-  char *s2 = "elkemmal";
+  char *s2 = "abdelhamid";
 
   printf("|%d|\n", ft_strlen(s2));
   return 0;
