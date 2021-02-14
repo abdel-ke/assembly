624477a0928a566defd61556110f223ddb6c84ff
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2021-02-14 17:20:26 +0100

    14/02/21

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,6 +1,6 @@
 section .text
 	global _ft_strdup
-	extern malloc
+	extern _malloc
 	extern _ft_strlen
 	extern _ft_strcpy
 _ft_strdup:
--- main.c
+++ main.c
@@ -10,18 +10,21 @@ char	*ft_strcpy(char *dest, char *src);
 int		ft_strcmp(char *s1, char *s2);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
-char *	strdup(const char *s1);
+char 	*ft_strdup(const char *s1);
 int main() {
 	// char *str;
 	// ft_strcpy(str, "|abdel-ke|");
 	// puts(str);
 	// printf("|%d|\n", ft_strlen("hamid"));
-	// printf("|%d|\n", ft_strcmp2("hamidkaml", "hamid"));
+	// printf("|%d|\n", ft_strcmp2("hamidkaml", "hamid"));}{}
 	// printf("|%d|\n",  ft_strcmp("hamidkaml", "hamid"));
 	// ft_write(1, "hamid\n", 6);
-	int	fd = open("ft_write.s", O_RDONLY);
-	char 	*new;
-	read(fd, new, 50);
-	ft_write(1, new, 50);
+	
+	
+	// int	fd = open("ft_write.s", O_RDONLY);
+	// char 	*new;
+	// read(fd, new, 50);
+	// ft_write(1, new, 50);
+	printf("|%s|\n", ft_strdup("hamid"));
 	return 0;
 }
